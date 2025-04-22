267907a814bc4785feb14622df8cd6da899e216f
Kody Deda <kodydeda4@gmail.com>
2025-04-22 11:26:07 -0400

    wip

--- Trivia/Trivia/TriviaApp.swift
+++ Trivia/Trivia/TriviaApp.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 import OpenTDB
 
+typealias Trivia = OpenTDBClient
+extension Trivia { static var shared = Trivia.previewValue }
+
 @main
 struct TriviaApp: App {
   var body: some Scene {
@@ -10,16 +13,3 @@ struct TriviaApp: App {
   }
 }
 
-// @DEDA
-// - [x] TrivaDb.previewValue's
-// - [x] formattedAnswer
-// - [x] preview value for quiz
-// - [x] preview value for quiz results
-// - [ ] remember to show logs in your presentation so you know the output...
-// - [ ] Git Readme similar to Cookable
-
-typealias Trivia = OpenTDBClient
-
-extension Trivia {
-  static var shared = Trivia.previewValue
-}
