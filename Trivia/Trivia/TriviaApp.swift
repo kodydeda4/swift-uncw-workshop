776c15d4a67676199fab6c647e809832c5a3f3b9
Kody Deda <kodydeda4@gmail.com>
2025-04-21 23:34:46 -0400

    init

---
+++ Trivia/Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Kody Deda on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
